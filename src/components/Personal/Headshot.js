801d1a57799ed24254390a829e9570b69e58861e
Graham <rescus1221@gmail.com>
2020-07-10 11:35:17 -0400

    🎨 Moved headshot image to Gravatar

--- src/components/Personal/Headshot.js
+++ src/components/Personal/Headshot.js
@@ -4,7 +4,7 @@ export const Headshot = () => {
   return (
     <div className='col-lg-3 d-flex justify-content-center'>
       <img
-        src='/headshot_400x400.jpg'
+        src='https://s.gravatar.com/avatar/dadb4b35780c36b7ac95bc67659f714e?s=500'
         alt='Headshot'
         className='mt-2 img-fluid rounded-circle'
       />
