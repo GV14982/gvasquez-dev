4186b6b30393aef1a52f9650af07b3e02eadd820
Graham Vasquez <graham@gvasquez.dev>
2026-01-14 16:31:24 -0500

    fix: spelling and grammar, ignore .astro folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import rehypeSlug from "rehype-slug";
+import rehypeHeadingLinks from "rehype-autolink-headings";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeHeadingLinks, {
+        behavior: 'prepend',
+        content: {
+          type: 'text',
+          value: '# ',
+        },
+      }]
+    ]
+  }
+});
